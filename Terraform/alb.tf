644bf2980082a21419219628bbd9f94fe2d7234c
Adminkeyan <kaishwarthi0321@gmail.com>
2025-06-18 15:09:09 +0530

    Create alb.tf

---
+++ Terraform/alb.tf
@@ -0,0 +1,27 @@
+
+resource "aws_lb" "app_lb" {
+  name               = "ror-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  name        = "ror-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
