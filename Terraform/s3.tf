a2aa904511748d778dd11a2f6ac28bfdec39ff41
Adminkeyan <kaishwarthi0321@gmail.com>
2025-06-18 15:13:02 +0530

    Create s3.tf

---
+++ Terraform/s3.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "ror-app-bucket-${random_id.bucket_id.hex}"
+  force_destroy = true
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
